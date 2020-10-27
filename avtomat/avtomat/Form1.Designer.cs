485089eae829af5e49c5b62c13f51e9b5b11624a
tbapb322 <56725727+tbapb322@users.noreply.github.com>
2020-10-27 14:55:57 +0500

    Add files via upload

---
+++ avtomat/avtomat/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace avtomat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Red;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(237, 450);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 56);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "вставить жетон";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.button1_MouseClick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Red;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(94, 459);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 51);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "--";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.DodgerBlue;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(440, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(237, 31);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "ожидание жетона";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Firebrick;
+            this.button2.Enabled = false;
+            this.button2.ForeColor = System.Drawing.Color.Black;
+            this.button2.Location = new System.Drawing.Point(503, 450);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(57, 51);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "<= left";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Firebrick;
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(556, 450);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(55, 51);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "right =>";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.button3.KeyDown += new System.Windows.Forms.KeyEventHandler(this.button3_KeyDown);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.pictureBox1.BackgroundImage = global::avtomat.Properties.Resources.craww;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox1.ErrorImage = global::avtomat.Properties.Resources.craww;
+            this.pictureBox1.InitialImage = global::avtomat.Properties.Resources.craww;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 40);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(112, 100);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.UseWaitCursor = true;
+            this.pictureBox1.WaitOnLoad = true;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button4.Enabled = false;
+            this.button4.Location = new System.Drawing.Point(657, 441);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(52, 69);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "подобрать";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.pictureBox2.BackgroundImage = global::avtomat.Properties.Resources.bearи;
+            this.pictureBox2.Location = new System.Drawing.Point(375, 375);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(48, 50);
+            this.pictureBox2.TabIndex = 11;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox3.Image = global::avtomat.Properties.Resources.s;
+            this.pictureBox3.Location = new System.Drawing.Point(3, 375);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox3.TabIndex = 12;
+            this.pictureBox3.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.LightCoral;
+            this.label1.Location = new System.Drawing.Point(72, 441);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "оставшееся время";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Red;
+            this.BackgroundImage = global::avtomat.Properties.Resources.eart23;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label1;
+        protected internal System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ avtomat/avtomat/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace avtomat
+{
+    
+    public partial class Form1 : Form
+    {       
+        Image craw;
+        public Form1()
+        {
+            InitializeComponent();
+           
+            pictureBox1.Image = craw;
+        }
+        int time = 0;
+        int currentState;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            currentState = 1;
+        }
+
+        private void button1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (currentState == 1 )
+            {
+                time = 30;
+                label3.Text = "Режим: ожидание действия";
+                label2.Text = time.ToString();
+            }
+            button2.Enabled = true;
+            button3.Enabled = true;
+            button4.Enabled = true;
+            pictureBox2.Left = 375;
+            currentState = 2;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Left-20>=0)
+                pictureBox1.Left -= 20;
+            timer1.Enabled = true;
+            currentState = 3;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Right + 20 <= 800)
+                pictureBox1.Left += 20;
+            timer1.Enabled = true;
+            currentState = 3;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            currentState = 1;
+            while (pictureBox1.Bottom <= pictureBox2.Bottom)
+            {
+                pictureBox1.Top += 7;
+            }
+            pictureBox1.Top += 1;
+
+            if (pictureBox2.Left >= pictureBox1.Left && pictureBox2.Right <= pictureBox1.Right)
+            {
+                while (pictureBox2.Top >= label3.Bottom)
+                {
+                    pictureBox2.Top -= 7;
+                    pictureBox1.Top -= 7;
+                }
+                while (pictureBox2.Left >= 0)
+                {
+                    pictureBox2.Left -= 7;
+                    pictureBox1.Left -= 7;
+                }
+                
+                while (pictureBox2.Bottom <= pictureBox3.Bottom)
+                {
+                    pictureBox2.Top += 7;
+                }
+                label3.Text = "Режим: ожидание жетона";
+                timer1.Enabled = false;
+                button2.Enabled = false;
+                button3.Enabled = false;
+                button4.Enabled = false;
+                label2.Text = "--";
+            }
+            else
+            {
+                label3.Text = "Режим: ожидание жетона";
+                timer1.Enabled = false;
+                button2.Enabled = false;
+                button3.Enabled = false;
+                button4.Enabled = false;
+                label2.Text = "--";
+                while (pictureBox1.Top >= label3.Bottom)
+                {
+                    pictureBox1.Top -= 5;
+                }
+                while(pictureBox1.Left >= 0)
+                {
+                    pictureBox1.Left -= 5;
+                }
+               
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label3.Text = "режим: управления";
+            label2.Text = time.ToString();
+            time--;
+            if (time <= 0)
+            {                
+                label3.Text = "Режим: ожидание жетона";
+                timer1.Enabled = false;
+                button2.Enabled = false;
+                button3.Enabled = false;
+                button4.Enabled = false;
+                label2.Text = "--";
+            }
+
+        }        
+    }
+}
